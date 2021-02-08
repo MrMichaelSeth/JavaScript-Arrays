bbaeb5872b774110f9bbfa8baec594fa350f865f
Your Name <you@example.com>
2021-02-08 11:12:02 -0500

    added array and index file

---
+++ array.js
@@ -0,0 +1,8 @@
+console.log("connected...")
+/* Create an array of students scores
+loop each value of arrays and post to webpage
+each value of scores in array should post on a new line
+Create a variable that displays number of students and print on webpage
+Print number of studnets to webpage before grades on a new line 
+
+*/
