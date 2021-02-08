283233315d75f0d0a80e2d938d2aae97985a6ec5
Michael Seth <michael.a.seth175@gmail.com>
2021-02-08 13:33:46 -0500

    added random scores and HTML display

--- array.js
+++ array.js
@@ -4,5 +4,20 @@ loop each value of arrays and post to webpage
 each value of scores in array should post on a new line
 Create a variable that displays number of students and print on webpage
 Print number of studnets to webpage before grades on a new line 
-
 */
+
+let scores = [ ];
+let scoresLength = 20; 
+for ( let i = 0; i < scoresLength; i++) {
+    scores.push(Math.floor(Math.random() * 100) + 1);
+}
+console.log(scores);
+
+
+let main;
+for ( let i = 0; i < scores.length; i++) {
+    main += `Score: ${scores[i]} <br>`;
+}
+
+document.getElementById("output").innerHTML = main;
+document.getElementById("output").innerHTML += `# of scores: ${scores.length}`;
