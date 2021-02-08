cde932ae15c11d4598a72a45821d88fabbd44af9
Your Name <you@example.com>
2021-02-08 14:28:43 -0500

    added highest and lowest

--- array.js
+++ array.js
@@ -20,4 +20,14 @@ for ( let i = 0; i < scores.length; i++) {
 }
 
 document.getElementById("output").innerHTML = main;
-document.getElementById("output").innerHTML += `# of scores: ${scores.length}`;
+document.getElementById("output").innerHTML += `# of scores: ${scores.length}<br>`;
+
+/* define highest and lowest number*/
+let lowest = 101
+let highest = 0; 
+for ( let i = 0; i < scores.length; i++)  {
+    if(scores[i]>highest){highest = scores[i]}
+    if(scores[i]<lowest){lowest = scores[i]}
+}
+document.getElementById("output").innerHTML += `highest score: ${highest}<br>`;
+document.getElementById("output").innerHTML += `lowest score: ${lowest}<br>`;
